e8b99eae53e308e053f9ab7a892e9d0e6fc47e84
Andrew Vagliano <andrew.vagliano1@gmail.com>
2025-12-05 10:47:54 -0500

    Update create_day script with new Solution structure

--- create_day.sh
+++ create_day.sh
@@ -16,32 +16,24 @@ curl -b "session=$AOC_COOKIE" https://adventofcode.com/$year/day/$day/input > "$
 # Generate boilerplate script
 cat > "$year/day$day/run.py" << 'EOF'
 from typing import List
+from solution import BaseSolution
 
 
-class Solution:
-    INPUT_FILE = "./input.txt"
-    EXAMPLE_FILE = "./example.txt"
+class Solution(BaseSolution):
 
-    def __init__(self) -> None:
+    def init(self) -> None:
         self.lines: List = []
-        with open(self.EXAMPLE_FILE, "r") as f:
+        with open(self.filename, "r") as f:
             for line in f.readlines():
+                line = line.strip()
                 self.lines.append(line)
 
     def stage1(self) -> int: ...
-
     def stage2(self) -> int: ...
 
 
-def main() -> None:
-    s = Solution()
-
-    print("Stage 1:", s.stage1())
-    print("Stage 2:", s.stage2())
-
-
 if __name__ == "__main__":
-    main()
+    Solution.main()
 EOF
 
 # Open challenge page
