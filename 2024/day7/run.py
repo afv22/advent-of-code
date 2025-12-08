6ba1bdb1692678731984a8aae5d6e4df69ea2e49
Andrew Vagliano <andrew.vagliano1@gmail.com>
2025-12-08 14:36:38 -0500

    2024 - Day 7

--- 2024/day7/run.py
+++ 2024/day7/run.py
@@ -1,28 +1,52 @@
-INPUT_FILE = "./input.txt"
-EXAMPLE_FILE = "./example.txt"
+from typing import List
+
+from aoc.base_solution import BaseSolution
+from aoc.io import IO
 
 
-def load_input() -> str:
-    with open(INPUT_FILE, "r") as f:
-        s = f.read()
+class Solution(BaseSolution):
 
-    return s
+    def init(self) -> None:
+        self.equations: List[tuple[int, List[int]]] = []
+        for line in IO.load_lines(self.filename):
+            target, raw_values = line.split(":")
+            values = map(int, raw_values.strip().split())
+            self.equations.append((int(target), list(values)))
 
+    @classmethod
+    def _check_values(cls, target: int, values: List[int], allow_concat: bool) -> bool:
+        def fn(tally, values):
+            if not values:
+                return target == tally
 
-def stage1(input) -> int:
-    return -1
+            value, values = values[0], values[1:]
+            if fn(tally + value, values):
+                return True
 
+            if fn(tally * value, values):
+                return True
 
-def stage2(input) -> int:
-    return -1
+            if allow_concat and fn(int(str(tally) + str(value)), values):
+                return True
 
+            return False
 
-def main() -> None:
-    input = load_input()
+        return fn(0, values)
 
-    print("Stage 1:", stage1(input))
-    print("Stage 2:", stage2(input))
+    def stage1(self) -> int:
+        total = 0
+        for target, values in self.equations:
+            if self._check_values(target, values, False):
+                total += target
+        return total
+
+    def stage2(self) -> int:
+        total = 0
+        for target, values in self.equations:
+            if self._check_values(target, values, True):
+                total += target
+        return total
 
 
 if __name__ == "__main__":
-    main()
+    Solution.main()
